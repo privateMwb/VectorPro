376d67856caec6cd866dc66f8d712b192cd41c51
Rain Lee <private.m.w.b.21@gmail.com>
2026-07-11 13:42:25 +0800

    chore: remove generated build artifacts

--- build-sanitize/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /data/data/com.termux/files/home/projects/C++/VectorPro
-# Build directory: /data/data/com.termux/files/home/projects/C++/VectorPro/build-sanitize
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[VectorProTests]=] "/data/data/com.termux/files/home/projects/C++/VectorPro/build-sanitize/tests")
-set_tests_properties([=[VectorProTests]=] PROPERTIES  _BACKTRACE_TRIPLES "/data/data/com.termux/files/home/projects/C++/VectorPro/CMakeLists.txt;87;add_test;/data/data/com.termux/files/home/projects/C++/VectorPro/CMakeLists.txt;0;")
