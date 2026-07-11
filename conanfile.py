1f429f264e4fe602bd98a1152149bf2cec19f408
Rain Lee <private.m.w.b.21@gmail.com>
2026-07-11 19:25:23 +0800

    build: add Conan package support

---
+++ conanfile.py
@@ -0,0 +1,62 @@
+from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+from conan.tools.files import copy
+import os
+
+class VectorProConan(ConanFile):
+    name = "vectorpro"
+    version = "1.0.0"
+
+    package_type = "header-library"
+
+    license = "MIT"
+    author = "privateMwb"
+    url = "https://github.com/privateMwb/VectorPro"
+    description = "High-performance std::vector-compatible contiguous container."
+    topics = (
+        "vector",
+        "container",
+        "header-only",
+        "cpp",
+        "data-structure"
+    )
+
+    settings = "os", "compiler", "build_type", "arch"
+
+    exports_sources = (
+        "include/*",
+        "cmake/*",
+        "CMakeLists.txt"
+    )
+
+    no_copy_source = True
+
+    def layout(self):
+        cmake_layout(self)
+
+    def package(self):
+        copy(
+            self,
+            "*.h", 
+            src=os.path.join(self.source_folder, "include"),
+            dst=os.path.join(self.package_folder, "include"),
+        )
+        copy(
+            self,
+            "*.hpp", 
+            src=os.path.join(self.source_folder, "include"),
+            dst=os.path.join(self.package_folder, "include"),
+        )
+        copy(
+            self,
+            "*.tpp", 
+            src=os.path.join(self.source_folder, "include"),
+            dst=os.path.join(self.package_folder, "include"),
+        )
+        
+    def package_info(self):
+        self.cpp_info.bindirs = []
+        self.cpp_info.libdirs = []
+
+    def package_id(self):
+        self.info.clear()
